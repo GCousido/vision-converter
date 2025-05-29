28181ec822ff3ade747dfd41966000bfaafdbfc6
Guillermo Cousido <guillermo.cousido@gmail.com>
2025-05-30 00:15:19 +0200

    add information and metadata to the project information

--- setup.py
+++ setup.py
@@ -3,14 +3,37 @@ from setuptools import setup, find_packages
 setup(
     name="datasetconverter",
     version="0.1.0",
+    description="This project consist of a library and a CLI for converting datasets between annotation formats.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Guillermo Cousido Martínez",
+    author_email="guillermo.cousido@gmail.com",
+    python_requires=">=3.12",
     packages=find_packages(),
     install_requires=[
-        "click",
-        "pillow"
+        "click>=8.1.8",
+        "pillow>=11.2.1",
+    ],
+    keywords=[
+        "dataset", "converter", "computer vision", "cli", "yolo", "coco", "pascal voc"
     ],
     entry_points={
-        'console_scripts': [
-            'dconverter=cli.main:dconverter',
+        "console_scripts": [
+            "dconverter=cli.main:dconverter",
         ],
     },
+    extras_require={
+        "dev": [
+            "pytest>=8.3.5",
+            "pytest-mock>=3.14.0",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    include_package_data=True,
 )
